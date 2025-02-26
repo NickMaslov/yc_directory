7a1c43e508d7843a297c28fb2c0f24e869fdbe99
Nick Maslov <kategorychnost@gmail.com>
2025-02-26 01:18:26 -0500

    Refactor layout and add Navbar component; implement global styles and custom fonts. Connected with GitHub OAuth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+export async function Navbar() {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} priority />
+        </Link>
+        <div className="flex gap-5 items-center text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name || "UserName"}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
